dacd1be01ad0fc7ca12906ae185c7501b13bb019
Manas Sharma <manas.sharma@thomsonreuters.com>
2020-10-03 05:25:03 +0800

    fix ios videos

---
+++ remove-audio-tracks.sh
@@ -0,0 +1,10 @@
+ffmpeg -i ./project-files/videos-compressed/cali-smoke.mp4 -an ./docs/assets/cali-smoke.mp4
+ffmpeg -i ./project-files/videos-compressed/cbse.mp4 -an ./docs/assets/cbse.mp4
+ffmpeg -i ./project-files/videos-compressed/clocks.mp4 -an ./docs/assets/clocks.mp4
+ffmpeg -i ./project-files/videos-compressed/head-count.mp4 -an ./docs/assets/head-count.mp4
+ffmpeg -i ./project-files/videos-compressed/herd.mp4 -an ./docs/assets/herd.mp4
+ffmpeg -i ./project-files/videos-compressed/himalayas.mp4 -an ./docs/assets/himalayas.mp4
+ffmpeg -i ./project-files/videos-compressed/hk-wiki.mp4 -an ./docs/assets/hk-wiki.mp4
+ffmpeg -i ./project-files/videos-compressed/india-pollution.mp4 -an ./docs/assets/india-pollution.mp4
+ffmpeg -i ./project-files/videos-compressed/singapore.mp4 -an ./docs/assets/singapore.mp4
+ffmpeg -i ./project-files/videos-compressed/wildlife.mp4 -an ./docs/assets/wildlife.mp4
