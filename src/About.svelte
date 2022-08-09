6f22e0a2f44a95364c202fd1e74767704af2d092
Manas Sharma <sharma.manas271196@gmail.com>
2022-08-10 00:09:23 +0530

    fix about

--- src/About.svelte
+++ src/About.svelte
@@ -3,17 +3,22 @@
 
 <div id="about-container">
   <p>
-    I was born and raised in the city of Lucknow, Uttar Pradesh.
+    I was born and raised in the city of Lucknow, Uttar Pradesh. Hopefully the city's tenderness is still a part of me.
   </p>
-  <p>I studied design at IIT, Guwahati.</p>
-  <p>
-    I have previously interned at the Info Design Lab at IDC, Hindustan
+  <p>I studied design at IIT Guwahati and have previously interned at the Info Design Lab at IDC, Hindustan
     Times, Scroll and The Washington Post.
   </p>
   <p>
     I've had the great fortune of learning from the most talented and supportive
-    people from the industry.
+    people in the industry.
   </p>
+  <p>
+    An incomplete list of insanely kind mentors who have always – knowingly or unknowingly – given me more than I have deserved: <br> <a href="https://twitter.com/SimonScarr">Simon Scarr</a> | <a href="https://twitter.com/monicaulmanu">Monica Ulmanu</a> | <a href="https://twitter.com/JonRMcClure">Jon McClure</a> | <a href="https://www.linkedin.com/in/matthew-weber-343ba4111/">Matthew Weber</a> | Jennifer T. | <a href="https://twitter.com/GurmanBhatia">Gurman Bhatia</a> | <a href="https://twitter.com/anandkatakam">Anand Katakam</a> | <a href="https://twitter.com/rishdotblog">Rishabh Srivastava</a> | <a href="https://twitter.com/Harry_Stevens">Harry Stevens</a> | <a href="http://www.idc.iitb.ac.in/venkat/index.html">Prof. Venkatesh Rajamanickam</a>
+  </p>
+  <p>
+    <span class="italic gray">"If I have seen further, it is by standing on the shoulders of giants"</span>
+  </p>
+  <p>I'm forever in their debt.</p>
 </div>
 
 <style>
@@ -23,5 +28,14 @@
     margin: 3rem auto;
     width: 90%;
     min-width: 300px;
+  }
+  .italic {
+    font-style: italic;
+  }
+  .gray {
+    color: #999;
+  }
+  a {
+    color: #3a3a3a;
   }
 </style>
